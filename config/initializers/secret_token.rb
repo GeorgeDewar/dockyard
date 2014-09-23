af21291928bd849afd822c1a95f03aeaad924432
George Dewar <george@dewar.co.nz>
2014-09-23 21:49:47 +1200

    Create shell Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dockyard::Application.config.secret_key_base = '4cdafa301bc1154fdbbb117c22bc60b474e0d9c59c16a37a826e1c4852d090670db2b4b2730456e2d5ef106d94dde2985a3f16f41107030c67bc98101bb1d0f0'
