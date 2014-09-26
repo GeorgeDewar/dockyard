23d7f6290fb34f04dc27bb0500162866e6947793
George Dewar <george.dewar@datacom.co.nz>
2014-09-26 16:04:39 +1200

    Resource route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Dockyard::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :repositories
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
